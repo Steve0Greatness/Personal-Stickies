49763a6941696d0e2ff9e2d62d7673fa39888482
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2022-05-17 00:51:54 +0000

    closing the preview, and Prettier

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express'),
 	app = express(),
 	cp = require('cookie-parser'),
 	uuids = require('store'),
-	fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args)),
+	fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args)),
 	fs = require('fs'),
 	cors = require('cors');
-require("ejs");
+require('ejs');
 
 app.use(cp());
 app.use(express.static('static'));
@@ -16,7 +16,7 @@ app.use(async (req, res, next) => {
 		res.clearCookie('uuid');
 	}
 	setTimeout(next, 150);
-})
+});
 
 function gobackhome(res, time = 150) {
 	setTimeout(() => res.redirect('/'), time);
@@ -33,36 +33,36 @@ try {
 
 app.get('/api/auth', (req, res) => {
 	fetch(`https://auth.itinerary.eu.org/api/auth/verifyToken?privateCode=${req.query.privateCode}`)
-		.then(s => s.json())
-		.then(d => {
+		.then((s) => s.json())
+		.then((d) => {
 			if (!d.valid) {
 				res.redirect('/');
 				return;
 			}
 			let uuid = crypto.randomUUID();
-			uuids.set(uuid, d.username)
-			res.cookie('uuid', uuid, { maxAge: 24*60*60*1000, httpOnly: true });
-			gobackhome(res)
-		})
+			uuids.set(uuid, d.username);
+			res.cookie('uuid', uuid, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+			gobackhome(res);
+		});
 });
 
 app.get('/logout', (req, res) => {
 	uuids.remove(req.cookies.uuid);
 	res.clearCookie('uuid');
-	gobackhome(res)
+	gobackhome(res);
 });
 
 app.get('/api/parseURL', (req, res) => {
 	let url = new URL(req.query.url);
-	url = url.pathname.split("/")
-	res.redirect(`/api/add?topicId=${url[3]}`)
-})
+	url = url.pathname.split('/');
+	res.redirect(`/api/add?topicId=${url[3]}`);
+});
 
 app.get('/api/parseURL_', (req, res) => {
 	let url = new URL(req.query.url);
-	url = url.pathname.split("/")
-	res.redirect(`/api/remove?topicId=${url[3]}`)
-})
+	url = url.pathname.split('/');
+	res.redirect(`/api/remove?topicId=${url[3]}`);
+});
 
 app.get('/api/add', (req, res) => {
 	fs.readFile(__dirname + '/users.json', (err, data) => {
@@ -71,54 +71,58 @@ app.get('/api/add', (req, res) => {
 			return;
 		}
 		let body = JSON.parse(data);
-		if (uuids.get(req.cookies.uuid) === undefined || !("uuid" in req.cookies)) {
-			gobackhome(res)
+		if (uuids.get(req.cookies.uuid) === undefined || !('uuid' in req.cookies)) {
+			gobackhome(res);
 			return;
 		}
 		let user = uuids.get(req.cookies.uuid);
 		if (!(user.toLowerCase() in body)) {
 			fetch(`https://scratchdb.lefty.one/v3/user/info/${user}`)
-				.then(s => s.json())
-				.then(d => {
+				.then((s) => s.json())
+				.then((d) => {
 					body[user.toLowerCase()] = {
 						user: {
 							id: d.id,
-							name: user
+							name: user,
 						},
-						stickies: []
+						stickies: [],
 					};
 					fetch(`https://scratchdb.lefty.one/v3/forum/topic/posts/${req.query.topicId}/0?o=oldest`)
-						.then(s => s.json())
-						.then(b => {
+						.then((s) => s.json())
+						.then((b) => {
 							body[user.toLowerCase()].stickies.push({
 								topic_ID: b[0].topic.id,
 								topic_NAME: b[0].topic.title,
-								topic_OWNER: b[0].username
-							})
-							fs.writeFile(__dirname + "/users.json", JSON.stringify(body), (e) => {
-								console.error(e)
+								topic_OWNER: b[0].username,
 							});
-							gobackhome(res, 150)
-						})
-				})
+							fs.writeFile(
+								__dirname + '/users.json',
+								JSON.stringify(body),
+								(e) => {
+									console.error(e);
+								}
+							);
+							gobackhome(res, 150);
+						});
+				});
 		} else {
 			fetch(`https://scratchdb.lefty.one/v3/forum/topic/posts/${req.query.topicId}/0?o=oldest`)
-				.then(s => s.json())
-				.then(b => {
+				.then((s) => s.json())
+				.then((b) => {
 					body[user.toLowerCase()].stickies.push({
 						topic_ID: b[0].topic.id,
 						topic_NAME: b[0].topic.title,
-						topic_OWNER: b[0].username
-					})
-					let write = new Uint8Array(Buffer.from(JSON.stringify(body)));
-					fs.writeFile(__dirname + "/users.json", write, (e) => {
-						console.error(e)
+						topic_OWNER: b[0].username,
 					});
-					gobackhome(res, 150)
-				})
+					let write = new Uint8Array(Buffer.from(JSON.stringify(body)));
+					fs.writeFile(__dirname + '/users.json', write, (e) => {
+						console.error(e);
+					});
+					gobackhome(res, 150);
+				});
 		}
-	})
-})
+	});
+});
 
 app.get('/api/remove', (req, res) => {
 	fs.readFile(__dirname + '/users.json', (err, data) => {
@@ -126,21 +130,23 @@ app.get('/api/remove', (req, res) => {
 			console.error(err);
 			return;
 		}
-		if (uuids.get(req.cookies.uuid) === undefined || !("uuid" in req.cookies)) {
-			gobackhome(res)
+		if (uuids.get(req.cookies.uuid) === undefined || !('uuid' in req.cookies)) {
+			gobackhome(res);
 			return;
 		}
 		let body = JSON.parse(data),
 			user = uuids.get(req.cookies.uuid);
-		body[user.toLowerCase()].stickies = body[user.toLowerCase()].stickies.filter(e => e.topic_ID !== parseInt(req.query.topicId))
-		fs.writeFile(__dirname + "/users.json", JSON.stringify(body), (err) => {
+		body[user.toLowerCase()].stickies = body[
+			user.toLowerCase()
+		].stickies.filter((e) => e.topic_ID !== parseInt(req.query.topicId));
+		fs.writeFile(__dirname + '/users.json', JSON.stringify(body), (err) => {
 			if (err) {
-				console.error(err)
+				console.error(err);
 			}
-		})
-		gobackhome(res)
-	})
-})
+		});
+		gobackhome(res);
+	});
+});
 
 app.get('/signin', (req, res) => {
 	let red = Buffer.from('https://Personal-Stickies.stevesgreatness.repl.co/api/auth', 'utf-8').toString('base64');
@@ -152,7 +158,7 @@ app.get('/add', (req, res) => {
 		res.clearCookie('uuid');
 		gobackhome(res);
 	}
-	setTimeout(() => res.sendFile(__dirname + '/preview.html'), 1000)
+	setTimeout(() => res.sendFile(__dirname + '/preview.html'), 1000);
 });
 
 app.get('/remove', (req, res) => {
@@ -160,31 +166,35 @@ app.get('/remove', (req, res) => {
 		res.clearCookie('uuid');
 		gobackhome(res);
 	}
-	setTimeout(() => res.sendFile(__dirname + '/remove.html'), 1000)
+	setTimeout(() => res.sendFile(__dirname + '/remove.html'), 1000);
 });
 
 app.get('/', (req, res) => {
-	setTimeout(() => res.render(`index`, { bool: ('uuid' in req.cookies) }), 1000)
-})
+	setTimeout(() => res.render(`index`, { bool: 'uuid' in req.cookies }), 1000);
+});
 
 app.get('/users/:user', (req, res) => {
-	fs.readFile(__dirname + "/users.json", (err, data) => {
+	fs.readFile(__dirname + '/users.json', (err, data) => {
 		if (err) {
-			console.error(err)
+			console.error(err);
 			return;
 		}
 		let body = JSON.parse(data),
-			user = req.params.user
+			user = req.params.user;
 		if (!(user.toLowerCase() in body)) {
-			res.render("error", { code: 404, msg: "User Not Found", desc: "The requested user could not be found by the server." })
+			res.render('error', {
+				code: 404,
+				msg: 'User Not Found',
+				desc: 'The requested user could not be found by the server.',
+			});
 			return;
 		}
-		user = body[user.toLowerCase()]
+		user = body[user.toLowerCase()];
 		let name = user.user.name,
-			size = "16";
-		res.render('user', { name: name, user: user, size: size })
-	})
-})
+			size = '16';
+		res.render('user', { name: name, user: user, size: size });
+	});
+});
 
 app.get('/all_users', (req, res) => {
 	fs.readFile(__dirname + '/users.json', (err, data) => {
@@ -192,32 +202,32 @@ app.get('/all_users', (req, res) => {
 			throw err;
 		}
 		let body = JSON.parse(data);
-		setTimeout(() => { res.render('all_users', { body: body }) }, 100)
-	})
-})
+		setTimeout(() => {
+			res.render('all_users', { body: body });
+		}, 100);
+	});
+});
 
 app.get('/api/user_redirect', (req, res) => {
-	res.redirect(`/users/${req.query.user}`)
-})
+	res.redirect(`/users/${req.query.user}`);
+});
 
 app.get('/api/users/:user', cors(), (req, res) => {
 	fs.readFile(__dirname + '/users.json', (err, data) => {
-		if (err)
-			throw err;
+		if (err) throw err;
 		let body = JSON.parse(data),
 			user = req.params.user,
 			user_data = body[user.toLowerCase()];
 		res.send(user_data);
-	})
-})
+	});
+});
 
 app.get('/credits', (req, res) => {
 	fs.readFile(__dirname + '/static/contributers.json', (err, data) => {
-		if (err)
-			throw err;
+		if (err) throw err;
 		res.render('credits', { body: JSON.parse(data) });
-	})
-})
+	});
+});
 
 app.get('/me', (req, res) => {
 	if (!('uuid' in req.cookies) || uuids.get(req.cookies.uuid) === undefined) {
@@ -225,29 +235,30 @@ app.get('/me', (req, res) => {
 		return;
 	}
 	res.redirect(`/users/${uuids.get(req.cookies.uuid)}`);
-})
+});
 
 app.get('/me_embed', (req, res) => {
 	fs.readFile(__dirname + '/users.json', (err, data) => {
-		if (err)
-			throw err;
+		if (err) throw err;
 		let body = JSON.parse(data);
 		if (!('uuid' in req.cookies) || uuids.get(req.cookies.uuid) === undefined) {
-			res.redirect('/')
+			res.redirect('/');
 			return;
 		}
 		if (!(uuids.get(req.cookies.uuid).toLowerCase() in body)) {
-			res.send("")
+			res.send('');
 			return;
 		}
-		res.render('me', { stickies: body[uuids.get(req.cookies.uuid).toLowerCase()].stickies });
-	})
-})
+		res.render('me', {
+			stickies: body[uuids.get(req.cookies.uuid).toLowerCase()].stickies,
+		});
+	});
+});
 
 app.listen(3000, () => {
-	fs.readFile(__dirname + "/users.json", (err, data) => {
-		if (err)
-			throw err;
-		console.log(`There are ${Object.keys(JSON.parse(data)).length} user(s) in the DataBase`);
-	})
+	fs.readFile(__dirname + '/users.json', (err, data) => {
+		if (err) throw err;
+		console.log(`There are ${Object.keys(JSON.parse(data)).length} user(s) in the DataBase`
+		);
+	});
 });
--- static/preview.js
+++ static/preview.js
@@ -1,10 +1,16 @@
-const preview_location = document.querySelector("#preview")
+const preview_location = document.querySelector('#preview');
 function preview(id) {
 	fetch(`https://scratchdb.lefty.one/v3/forum/topic/posts/${id}/0?o=oldest`)
-		.then(res => res.json())
-		.then(data => {
+		.then((res) => res.json())
+		.then((data) => {
 			let topic = data[0].topic;
-			preview_location.innerHTML = `<img src="/sticky.png"> Sticky: <a href="${topic.id}">${topic.title}</a> by ${data[0].username}`
-			preview_location.parentElement.setAttribute("open", true)
-		})
+			preview_location.innerHTML = `<img src="/sticky.png"> Sticky: <a href="${topic.id}">${topic.title}</a> by ${data[0].username}`;
+			preview_location.parentElement.setAttribute('open', true);
+		});
 }
+
+preview_location.parentElement
+	.querySelector('#close_preview')
+	.addEventListener('click', () => {
+		preview_location.parentElement.removeAttribute('open');
+	});
